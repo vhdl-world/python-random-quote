97fa2716545eb92d6a2485a32e946c9cb8a9ef96
vhdl-world <62701334+vhdl-world@users.noreply.github.com>
2020-03-26 15:02:05 +0100

    Print out only one random quote

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
+import random
 def trial():
-  print("Keep it logically awesome.")
+  # print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes[0])
+  
+  last = len(quotes) - 1 
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   trial()
